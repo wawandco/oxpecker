7894e249971fff59e868afb67135f1f50aa6bd12
Antonio Pagano <apagano@wawand.co>
2020-11-10 20:50:45 -0500

    adding some tests

--- internal/plugins/tools/packr/build.go
+++ internal/plugins/tools/packr/build.go
@@ -2,14 +2,47 @@ package packr
 
 import (
 	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/gobuffalo/packr/v2/jam"
+	"github.com/paganotoni/x/internal/info"
 )
 
 // BeforeBuild pack
 func (w Plugin) RunBeforeBuild(ctx context.Context, root string, args []string) error {
-	// Generate file on the root to have Packr packs files correctly.
-	return nil
+	// check if there are Go files otherwise generate one
+	// to prevent Packr from failing
+	found := false
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if filepath.Dir(path) != root || filepath.Ext(path) != ".go" {
+			return nil
+		}
+
+		found = true
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// If found any .go in the root folder it will
+	// not do the rest of the things here.
+	if found {
+		return nil
+	}
+
+	name, err := info.BuildName()
+	if err != nil {
+		return err
+	}
+
+	filename := name + ".go"
+	content := "package " + name
+
+	return ioutil.WriteFile(filename, []byte(content), 0600)
 }
 
 // Build uses the Packr Jam library to generate packd folders
