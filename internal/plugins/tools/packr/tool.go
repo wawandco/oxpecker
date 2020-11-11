7db65a637fb09cc3d7c7ccc26d99b95708d85228
Antonio Pagano <apagano@wawand.co>
2020-11-10 20:54:47 -0500

    one minor compiler check

--- internal/plugins/tools/packr/tool.go
+++ internal/plugins/tools/packr/tool.go
@@ -1,5 +1,11 @@
 package packr
 
+import "github.com/paganotoni/x/internal/plugins"
+
+var (
+	_ plugins.Plugin = (*Plugin)(nil)
+)
+
 // Plugin provides operations with Plugin for the CLI.
 type Plugin struct{}
 
